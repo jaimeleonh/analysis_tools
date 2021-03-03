8a80634a4b5447b8e0e07e517bd3bb07a499c97c
Jaime Leon <jleonhol@gmail.com>
2021-03-03 15:54:05 +0100

    Included label atribute in the Dataset class

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 class Process():
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, label, **kwargs):
         self.name = name
+        self.label = label
         self.parent_process = None if "parent_process" not in kwargs else kwargs["parent_process"]
         self.subprocesses = []
         self.color = (0, 0, 0) if "color" not in kwargs else kwargs["color"]
